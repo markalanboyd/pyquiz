e75fdfdb884cd0dd00e57ad74e367473a427706d
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-24 08:03:37 +0100

    Update opentriviadb.py
    Added sev eral functions to get and parse categories as well as questions.

--- opentriviadb.py
+++ opentriviadb.py
@@ -1,9 +1,10 @@
 """
 Functions for accessing the Open Trivia Database API.
 """
-
-import requests
 import html
+import requests
+import urllib.parse
+
 
 def request_token() -> str:
     """Request a token from the Open Trivia Database API."""
@@ -11,6 +12,8 @@ def request_token() -> str:
     
     request = requests.get(API_REQUEST_URL)
     return request.json()['token']
+
+token = request_token()
 
 def request_categories() -> dict:
     """Request a list of categories from the Open Trivia Database API."""
@@ -26,17 +29,58 @@ def parse_categories(categories: dict) -> dict:
         parsed_categories[category['id']] = category['name']
     return parsed_categories
 
-def sort_dictionary(dictionary: dict) -> dict:
+def sort_dict_by_values(dictionary: dict) -> dict:
     """Sort a dictionary by its values."""
-    return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1])}
+    return dict(sorted(dictionary.items(), key=lambda item: item[1]))
 
 def values_to_list(dictionary: dict) -> list:
     """Convert a dictionary's values into a list."""
     return list(dictionary.values())
 
-token = request_token()
-categories = request_categories()
-categories = parse_categories(categories)
-categories = sort_dictionary(categories)
-categories = values_to_list(categories)
-print(categories)
+def swap_keys_values(dictionary: dict) -> dict:
+    """Swap a dictionary's keys and values."""
+    return {value: key for key, value in dictionary.items()}
+
+def list_categories() -> list:
+    """Return a list of categories."""
+    raw_categories = request_categories()
+    parsed_categories = parse_categories(raw_categories)
+    sorted_categories = sort_dict_by_values(parsed_categories)
+    categories_list = values_to_list(sorted_categories)
+    return categories_list
+
+def get_category_id(category: str) -> int:
+    """Return the ID of a category."""
+    raw_categories = request_categories()
+    parsed_categories = parse_categories(raw_categories)
+    sorted_categories = sort_dict_by_values(parsed_categories)
+    swapped_categories = swap_keys_values(sorted_categories)
+    return swapped_categories[category]
+
+def request_questions(token: str, 
+                      category: str = None,
+                      difficulty: str = '',
+                      type: str = '',
+                      amount: int = 1) -> dict:
+    """Request questions from the Open Trivia Database API."""
+    
+    params = {
+        'token': token,
+        'difficulty': difficulty,
+        'type': type,
+        'amount': amount,
+        }
+    
+    if category != None:
+        params['category'] = get_category_id(category)
+
+    encoded_params = urllib.parse.urlencode(params)
+    
+    API_REQUEST_URL = f'https://opentdb.com/api.php?{encoded_params}'
+    request = requests.get(API_REQUEST_URL)
+    return request.json()
+
+
+category = 'Animals'
+
+print(request_questions(token))
