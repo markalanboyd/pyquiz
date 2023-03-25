b22d46d1d325d9e825b277a9c2739f631c36f456
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-25 08:12:22 +0100

    Added game.html
    I think this is how Eel is supposed to work?

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import eel
 import opentriviadb as ot
 
 
-# Global constants
-
-
 # Global variables
 token = ot.request_token()
 
@@ -13,16 +10,10 @@ token = ot.request_token()
 # Functions
 def main():
     """Main function."""
-    
-    # questions = ot.request_questions(token, 
-    #                            ``      category=category, 
-    #                                  amount=2)
-    # ot.main_test(questions)
-    
     eel.init('web')
     
-    
-    
+    # Code Goes Here
+
     eel.start('index.html', size=(800, 600))
 
 if __name__ == '__main__':
