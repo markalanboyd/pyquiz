5cf8b0af5d63afb464d1beebedf3fc7829034af5
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-27 19:07:17 +0200

    Started work on CLI version using curses
    Maybe I got myself into another whole project with this, but hopign that curses will be relatively easy to use.

--- pyquiz_app/cli/pyquizcli/pyquizcli.py
+++ pyquiz_app/cli/pyquizcli/pyquizcli.py
@@ -1,3 +1,12 @@
-def hello_world():
-    print('Hello World!')
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Hello World", curses.color_pair(1))
+    stdscr.refresh()
+    stdscr.getch()
     
+
+wrapper(main)
